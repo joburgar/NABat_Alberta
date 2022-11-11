10bed679d31af18b596c1aeec08074895fbd776b
Joanna Burgar <joburgar@gmail.com>
2022-11-11 13:22:22 -0800

    2021 revisions

--- NABat_submission.R
+++ NABat_submission.R
@@ -11,8 +11,13 @@ if(length(new.packages)) install.packages(new.packages)
 
 lapply(list.of.packages, require, character.only = TRUE)
 
-# save.image("NABat_data_for_submission.RDS")
-# load("NABat_data_for_submission.RDS")
+# # Define the GRTS.Cell.ID and Year of interest if subsetting for year, studyarea
+# GRTS_interest <- "922"
+Year_interest <- year(as.Date("2021-01-01"))
+
+# depending on what has been previously been run, load either annual report data or submission data
+# load(paste("NABat_Annual_Report_",Year_interest,".RDS", sep=""))
+load(paste0("NABat_",Year_interest,"_data_for_submission.RDS"))
 
 ###- Appendix Table 3 - Nightly call counts
 ###---recode Classification to "AutoID" and use NABat 4-6 letter codes for NABat submission
@@ -25,6 +30,8 @@ lapply(list.of.packages, require, character.only = TRUE)
 #MYOVOL|MYOYUM|MYSE|MYSO|MYTH|MYVE|MYVO|MYYU|NOCLEP|NOISE|NOLE|NOTBAT|NYCFEM|NYCHUM|NYCMAC|NYFE|NYHU|NYMA|NYSP|NoID|PAHE|PARHES|PERSUB|PESU|STERUF|STRU|TABR|TADBRA|HiLo
 
 SA_call_data <- dat_time %>% select(Location.Name, Filename, Timep, SurveyNight, Classification)
+SA_call_data %>% filter(is.na(Timep)) # no NA Timep rows
+
 SA_call_data$Timep <- case_when(is.na(SA_call_data$Timep) ~
                                      as.POSIXct(strptime(substr(SA_call_data$Filename,19,24),tz=tz, format="%H%M%S")),
                                      TRUE ~ as.POSIXct(SA_call_data$Timep))
@@ -48,16 +55,29 @@ SA_call_data$SurveyDate <- case_when(SA_call_data$Timep > paste(Sys.Date(),"12:0
 SA_call_data$Time <- as.character(SA_call_data$Timep, "%H:%M:%S")
 
 SA_call_data$Classification <- as.factor(SA_call_data$Classification %>%
-                                        recode(`EPFU-LANO` = "EPFULANO", `LABO-MYLU` = "LABOMYLU", `Myotis 40k` = "40kMyo", MYEV.MYSE = "40kMyo",unknown = "NoID", noise = "NOISE"))
+                                        recode(`EPFU-LANO` = "EPFULANO", `LABO-MYLU` = "LABOMYLU", `Myotis 40k` = "40kMyo", MYEV.MYSE = "40kMyo",
+                                               LowF = "LoF", HighF = "HiF", unknown = "NoID", noise = "NOISE"))
+
+# to update MYEV and MYSE based on geography (N is MYSE and S is MYEV)
+SA_call_data$Classification <- case_when(SA_call_data$Classification=="MYEV-MYSE" & SA_call_data$Location.Name=="139715_NE_01" ~ "MYEV",
+                                          SA_call_data$Classification=="MYEV-MYSE" & SA_call_data$Location.Name!="139715_NE_01" ~ "MYSE",
+                                          TRUE ~ as.character(SA_call_data$Classification))
+
+SA_call_data %>% count(Classification)
 
 glimpse(SA_call_data)
 SA_call_data2 <- left_join(SA_call_data, Survey.Dates, by = c("SurveyYear", "Location.Name"))
+
+# if the weather is correctly captured from Alberta eBat output can simply use this code
+nightly.env.cov <- dat_summary %>% group_by(Location.Name,SurveyNight) %>%
+  summarise(across(min_temp:max_wind, ~mean(.x, na.rm=TRUE)))
+# summary(nightly.env.cov)
 
 Bulk_call_data <- left_join(SA_call_data2 %>% dplyr::select(-SurveyYear), nightly.env.cov, by = c("SurveyNight", "Location.Name"))
 Bulk_call_data$SurveyDate <- format(as.Date(Bulk_call_data$SurveyDate), '%m/%d/%Y')
 Bulk_call_data$`Audio Recording Time` <- paste(Bulk_call_data$SurveyDate, Bulk_call_data$Time)
 glimpse(Bulk_call_data)
-Bulk_call_data <- Bulk_call_data %>% select(Location.Name, Min.Tmp, Max.Tmp, Min.RH, Max.RH, Min.WS, Max.WS, Filename, `Audio Recording Time`, Classification, Survey.Start.Date, Survey.End.Date)
+Bulk_call_data <- Bulk_call_data %>% select(Location.Name, min_temp, max_temp, min_hum, max_hum, min_wind, max_wind, Filename, `Audio Recording Time`, Classification, Survey.Start.Date, Survey.End.Date)
 
 
 colnames(Bulk_call_data) <- c("Location Name", "Nightly Low Temperature", "Nightly High Temperature", "Nightly Low Relative Humidity", "Nightly High Relative Humidity",
@@ -95,7 +115,6 @@ Bulk_call_data$`Survey Start Time` <- paste(format(as.Date(Bulk_call_data$`Surve
 Bulk_call_data$`Survey End Time` <- paste(format(as.Date(Bulk_call_data$`Survey End Time`), '%m/%d/%Y'), "12:00:00")
 glimpse(Bulk_call_data)
 
-# also have bulk call meta for all stations outside of NP (except Elk Island)
 ###--- If want to exclude submissions based within some NP
 # NABat_NPsubmit <- eff %>% filter(Land.Unit.Code %in% c("BANP","JANP","WLNP")) %>% count(Location.Name)
 # Bulk_call_data_tosubmit <- Bulk_call_data %>% filter(!`Location Name` %in% NABat_NPsubmit$Location.Name)
@@ -110,9 +129,37 @@ for(i in seq_along(Year_interest)){
 
 ###--- Bulk Stationary Acoustic Meta Template
 #- provide contacts with bulk data for review and reference
+Appendix.Table1 <- sta[c("GRTS.Cell.ID","Location.Name", "Orig_Name","Latitude", "Longitude","Waterbody.Distance","Waterbody.Type","Road.Distance",
+                         "Road.Type", "Land.Use.Type","Land.Unit.Code")] 
+
+Appendix.Table1 <- left_join(Appendix.Table1, eff %>% select(Location.Name, Contact, Deployment.ID))
+Appendix.Table1 <- Appendix.Table1 %>% 
+  filter(Deployment.ID==Year_interest) %>%
+  dplyr::select(-Deployment.ID)
+
+Appendix.Table1 <- left_join(Appendix.Table1, Survey.Dates)
 # glimpse(Appendix.Table1)
 # issues when exported = 1. Developed (need to change to Urban); 2. Forest-mixed not accepted (change to Forest-conifer); 3. BANP GRTS as 132485 should be 132458
-Bulk_site_meta <- Appendix.Table1 %>% select(-Road.Distance,-Road.Type,-Human.Footprint.Distance,-Human.Footprint.Sublayer,-Human.Footprint.Type, -Orig.Name)
+
+# add in null columns (NABat template)
+xx <- c("Detector","Detector.Serial.Number", "Microphone","Microphone.Serial.Number","Microphone.Orientation",
+        "Microphone.Height","Clutter.Distance","Clutter.Type","Percent.Clutter","Weather.Proofing","Unusual.Occurrences")
+Appendix.Table1[xx] <- as.character(NA)
+
+# put in the same order as NABat template
+names(Appendix.Table1)
+Appendix.Table1 <- Appendix.Table1[c("GRTS.Cell.ID","Location.Name", "Orig_Name","Latitude", "Longitude","Survey.Start.Date", 
+                                     "Survey.End.Date", "Detector", "Detector.Serial.Number", "Microphone","Microphone.Serial.Number",
+                                     "Microphone.Orientation","Microphone.Height","Clutter.Distance","Clutter.Type",
+                                     "Waterbody.Distance","Waterbody.Type","Percent.Clutter","Road.Distance","Road.Type",
+                                     "Land.Use.Type","Land.Unit.Code","Contact","Weather.Proofing","Unusual.Occurrences")]
+
+Appendix.Table1 %>% count(Land.Use.Type)
+Appendix.Table1$Land.Use.Type <- Appendix.Table1$Land.Use.Type %>% recode(Developed = "Urban", `Forest-mixed` = "Forest-conifer") %>% tolower()
+
+Bulk_site_meta <- Appendix.Table1 %>% select(-Road.Distance,-Road.Type, -Orig_Name)
+names(Bulk_site_meta)
+glimpse(Bulk_site_meta)
 colnames(Bulk_site_meta) <- c("| GRTS Cell Id", "Location Name","Latitude", "Longitude", "Survey Start Time", "Survey End Time", "Detector", "Detector Serial Number",
                                "Microphone", "Microphone Serial Number", "Microphone Orientation", "Microphone Height (meters)", "Distance to Nearest Clutter (meters)", "Clutter Type",
                                "Distance to Nearest Water (meters)", "Water Type", "Percent Clutter", "Broad Habitat Type", "Land Unit Code","Contact","Weather Proofing", "Unusual Occurrences" )
@@ -124,45 +171,33 @@ Bulk_site_meta[cols.as.numeric]<- sapply(Bulk_site_meta[cols.as.numeric],as.nume
 glimpse(Bulk_site_meta)
 nrow(Bulk_site_meta)
 # Export
-write.table(Bulk_site_meta, "NABat_submit/Bulk_site_meta.csv",na = "",row.names = FALSE,sep = ",")
+write.table(Bulk_site_meta, "NABat_submit/Bulk_site_meta_2021.csv",na = "",row.names = FALSE,sep = ",")
 
 # write.table(Bulk_site_meta, "Bulk_site_meta.csv",na = "",row.names = FALSE,sep = ",")
 
 
 # # Write files for bio review
 # Export in bio batches
-bio_contact <- c("barb", "Brett", "hurtado","hughes","olson","david","helena","Unrhuh","lisa","Steenweg","saakje","sandi")
+Bulk_site_meta %>% count(Contact)
+bio_contact <- c("barb", "Brett", "hurtado","hughes","olson","david","helena","Unrhuh","julie","lisa","Steenweg","saakje","sandi","sharon")
 for(i in seq_along(bio_contact)){
-  write.table(Bulk_site_meta %>% filter(grepl(bio_contact[i],Contact)),paste0("NABat_submit/Bulk_site_meta_",bio_contact[i],".csv"), na = "",row.names = FALSE,sep = ",")
+  write.table(Bulk_site_meta %>% filter(grepl(bio_contact[i],Contact)),paste0("NABat_submit/Bulk_site_meta_2021_",bio_contact[i],".csv"), na = "",row.names = FALSE,sep = ",")
 }
 
 ###--- Bulk Stationary Acoustic Data Template
 #- provide contacts with bulk data for review and reference
-# glimpse(Bulk_call_data)
-# 
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("barb", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_BNP.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("Brett", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_LAR.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("hurtado", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_UofA.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("hughes", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_PR1.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("olson", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_Cori.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("david", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_EINP.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("helena", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_WLNP.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("Unruh", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_RDR.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("lisa", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_Lisa.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("Steenweg", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_PR2.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("saakje", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_JNP.csv", na = "",row.names = FALSE,sep = ",")
-# write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl("sandi", contact_cells$Contact),]$`Location Name`),
-#           "Bulk_call_data_MH.csv", na = "",row.names = FALSE,sep = ",")
+glimpse(Bulk_call_data)
+contact_cells <- Bulk_site_meta %>% select(Contact, `Location Name`)
+
+for(i in seq_along(bio_contact)){
+  write.table(Bulk_call_data %>% filter(`Location Name` %in% contact_cells[grepl(bio_contact[i], contact_cells$Contact),]$`Location Name`),
+              paste0("NABat_submit/Bulk_ACA_call_data_2021_",bio_contact[i],".csv"), 
+              na = "",row.names = FALSE,sep = ",")
+}
+
+rm(list=setdiff(ls(), c("Bulk_call_data","Bulk_site_meta")))
+
+# Year_interest <- year(as.Date("2021-01-01"))
+# save.image(paste0("NABat_",Year_interest,"_data_for_submission.RDS"))
 
 ##############################################################################################
